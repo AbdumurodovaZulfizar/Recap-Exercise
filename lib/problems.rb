57c22d10aebd8b095138708a9599c1652a3cffcb
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-21 15:36:25 +0500

    Create instructions for array problems

--- lib/problems.rb
+++ lib/problems.rb
@@ -56,4 +56,26 @@ class Hash
   end
 end
 
-{"a"=>1, "b"=>2}.inverse
+class Array
+  # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
+  def pair_sum_count(num)
+
+  end
+
+  # Write a method, Array#bubble_sort, that takes in an optional proc argument.
+  # When given a proc, the method should sort the array according to the proc.
+  # When no proc is given, the method should sort the array in increasing order.
+  #
+  # Sorting algorithms like bubble_sort, commonly accept a block. That block accepts
+  # two parameters, which represents the two elements in the array being compared.
+  # If the block returns 1, it means that the second element passed to the block
+  # should go before the first (i.e. switch the elements). If the block returns -1,
+  # it means the first element passed to the block should go before the second
+  # (i.e. do not switch them). If the block returns 0 it implies that
+  # it does not matter which element goes first (i.e. do nothing).
+  #
+  # This should remind you of the spaceship operator! Convenient :)
+  def bubble_sort(&prc)
+
+  end
+end
