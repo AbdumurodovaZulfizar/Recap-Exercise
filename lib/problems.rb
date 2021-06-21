5d6ee537218cf9f012f7fdb406f8a13bb8f55400
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-21 12:51:40 +0500

    Add instructions for least common multiply

---
+++ lib/problems.rb
@@ -0,0 +1,5 @@
+# Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
+# of both of the given numbers
+def least_common_multiple(num_1, num_2)
+
+end
